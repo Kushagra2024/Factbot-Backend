5d266f4cd5db59e82850fe53d6ce2c943e44a0cb
Kushagra Agrawal <kushagrawal202@gmail.com>
2025-06-18 16:57:12 +0530

    added current environment

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ import { configDotenv } from "dotenv";
 configDotenv({ path: "./.env" });
 
 const _CONFIG = Object.freeze({
+    ENV: String(process.env.ENV),
     PORT: Number(process.env.PORT),
     MONGODB_URI: String(process.env.MONGODB_URI),
     ORIGIN:
