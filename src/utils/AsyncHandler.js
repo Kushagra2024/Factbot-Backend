d8ebe1444abff8aa4c2e37c8f41749a6be02c0a1
Kushagra Agrawal <kushagrawal202@gmail.com>
2025-06-22 18:43:52 +0530

    Added Async Handler

---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,9 @@
+function AsyncHandler(requestHandler) {
+    return function (req, res, next) {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+            next(error)
+        );
+    };
+}
+
+export default AsyncHandler;
